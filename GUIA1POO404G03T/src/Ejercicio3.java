54d9e1a74077d915e32f95fdd5aa316c37b92f3a
Rafael Alejandro Escorcia Siliézar <rafaelitoescorcia@gmail.com>
2025-02-28 17:16:02 -0600

    Update and rename AlquilerJets.java to Ejercicio3.java

--- GUIA1POO404G03T/src/Ejercicio3.java
+++ GUIA1POO404G03T/src/Ejercicio3.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException; //<-- sirve para la lectura de la entra
 import java.util.Scanner;
 
 
-public class AlquilerJets {
+public class Ejercicio3 {
 
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
